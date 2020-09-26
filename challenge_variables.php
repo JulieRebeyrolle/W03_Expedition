661fc2bdb9ee0e9323d874d15a9a73e68b89e984
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-09-27 01:31:09 +0200

    ajout exercice variables

--- challenge_variables.php
+++ challenge_variables.php
@@ -1,3 +1,12 @@
 <?php
+$movieTitle = "Indiana Jones and the last crusade";
+$movieAlreadySeen = true;
+$movieReleaseYear = 1989;
+$movieImdbRating = 8.2;
 
+echo $movieTitle ."<br>";
+echo $movieAlreadySeen."<br>";
+echo $movieReleaseYear."<br>";
+echo $movieImdbRating."<br>";
 
+?>
