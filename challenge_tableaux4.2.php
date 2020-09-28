e1b621e84ed62a00ed6bec00b266a78d6cd3546e
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-09-28 13:30:11 +0200

    challenge 4.2

--- challenge_tableaux4.2.php
+++ challenge_tableaux4.2.php
@@ -1,1 +1,21 @@
 <?php
+/*$weapons = [
+    'Indiana Jones' => ['whip', 'gun', 'saber'],
+    'Marion Ravenwood' => ['knife', 'shield'],
+    'Helen Seymour' => ['belt', 'dagger', 'gun', 'shield']
+];*/
+
+$indianaJonesMovies = [
+    'Les aventuriers de l\'arche perdue' => ['Harrison Ford', 'Karen Allen', 'Paul Freeman'],
+    'Indiana Jones et le temple maudit' => ['Harrison Ford', 'Kate Capshaw', 'Ke Huy Quan'],
+    'Indiana Jones et la dernière croisade' => ['Harrison Ford', 'Sean Connery', 'Alison Doody']
+];
+
+foreach ($indianaJonesMovies as $title => $actors){
+    echo 'Dans le film ' . $title . ' les principaux acteurs sont :';
+
+    foreach ($actors as $actor){
+        echo ' ' . $actor;
+    }
+    echo '.' . "\n";
+}
